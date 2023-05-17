d2a80024d1c238a776ab353f4a8ffbda89d881f7
MyMOMHatesMe <133900325+MyMOMHatesMe@users.noreply.github.com>
2023-05-17 22:13:29 +0530

    Update Check_Number.cpp

--- Check_Number.cpp
+++ Check_Number.cpp
@@ -2,44 +2,44 @@
 
 
 
-ASK/VIEW DOUBT
+// ASK/VIEW DOUBT
 
 
-SOLUTION
+// SOLUTION
 
 
-HINT
+// HINT
 
-Problem
-Result
-Check Number
-Send Feedback
-Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
-Do this recursively.
-Input Format :
-Line 1 : An Integer N i.e. size of array
-Line 2 : N integers which are elements of the array, separated by spaces
-Line 3 : Integer x
-Output Format :
-'true' or 'false'
-Constraints :
-1 <= N <= 10^3
-Sample Input 1 :
-3
-9 8 10
-8
-Sample Output 1 :
-true
-Sample Input 2 :
-3
-9 8 10
-2
-Sample Output 2 :
-false
+// Problem
+// Result
+// Check Number
+// Send Feedback
+// Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
+// Do this recursively.
+// Input Format :
+// Line 1 : An Integer N i.e. size of array
+// Line 2 : N integers which are elements of the array, separated by spaces
+// Line 3 : Integer x
+// Output Format :
+// 'true' or 'false'
+// Constraints :
+// 1 <= N <= 10^3
+// Sample Input 1 :
+// 3
+// 9 8 10
+// 8
+// Sample Output 1 :
+// true
+// Sample Input 2 :
+// 3
+// 9 8 10
+// 2
+// Sample Output 2 :
+// false
 
-Your Code
-C++ (g++ 5.4)
-12345678910111213
+// Your Code
+// C++ (g++ 5.4)
+// 12345678910111213
 bool checkNumber(int input[], int size, int x) {
   /* Don't write main().
      Don't read input, it is passed as function argument.
@@ -50,17 +50,18 @@ bool checkNumber(int input[], int size, int x) {
         return false;
     if(input[0]==x)
         return true;
+}
 
 
-CUSTOM INPUT
+// CUSTOM INPUT
 
 
-SUBMIT SOLUTION
+// SUBMIT SOLUTION
 
 
-PREVIOUS
+// PREVIOUS
 
 
-NEXT
+// NEXT
 
 
